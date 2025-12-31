7543d0349c112ee47bfab8f8847e9a83916d522b
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-12-31 17:37:10 +0100

    build: Run integration tests with itself

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     clearMocks: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'cobertura'],
+      reportsDirectory: './coverage',
+    },
   },
 })
